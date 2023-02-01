dfcf3766a4bfbd3ca49d0f13d6aa6541200ac7b9
‘charliewelland’ <welland@usc.eduqgit config --global user.email welland@usc.edu>
2023-01-31 17:07:54 -0800

    pythonclient

--- tcp_client.py
+++ tcp_client.py
@@ -8,11 +8,15 @@ use python "input->" function, enter a line of a few letters, such as "abcd"
 import socket
 
 def main():
-    # TODO: Create a socket and connect it to the server at the designated IP and port
+	HOST = "localhost"
+	PORT = 19000
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((HOST,PORT))
+		s.sendall(b"Hello, world")
+		data = s.recv(1024)
+	print(f"Received {data!r}")   
+# TODO: Create a socket and connect it to the server at the designated IP and port
     # TODO: Get user input and send it to the server using your TCP socket
     # TODO: Receive a response from the server and close the TCP connection
-    pass
-
-
 if __name__ == '__main__':
     main()
