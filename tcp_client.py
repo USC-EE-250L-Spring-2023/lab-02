eda719a4bb93fd1a3ed473aff717443d4a86028e
‘charliewelland’ <welland@usc.eduqgit config --global user.email welland@usc.edu>
2023-02-07 15:20:03 -0800

    python

--- tcp_client.py
+++ tcp_client.py
@@ -12,9 +12,13 @@ def main():
 	PORT = 19000
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST,PORT))
-		s.sendall(b"Hello, world")
+		message = str(input("Enter a message: "))
+		s.sendall(message.encode("utf-8"))
 		data = s.recv(1024)
-	print(f"Received {data!r}")   
+		s.close()
+		decoded = data.decode("utf-8")
+	print(f"Received {decoded!r}")   
+
 # TODO: Create a socket and connect it to the server at the designated IP and port
     # TODO: Get user input and send it to the server using your TCP socket
     # TODO: Receive a response from the server and close the TCP connection
